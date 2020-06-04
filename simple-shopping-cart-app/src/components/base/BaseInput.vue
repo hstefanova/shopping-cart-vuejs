d0b4e550c8464a2990e2b384be3114f513368ebd
hstefanova <hstefanova@github.com>
2020-06-04 18:28:36 +0300

    form validation wip

--- simple-shopping-cart-app/src/components/base/BaseInput.vue
+++ simple-shopping-cart-app/src/components/base/BaseInput.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="field">
     <label v-if="label"> {{ label }}</label>
-    <input :value="value" @input="updateValue" v-bind="$attrs" />
+    <input
+      :value="value"
+      @input="updateValue"
+      v-bind="$attrs"
+      v-on="listeners"
+    />
   </div>
 </template>
 
@@ -14,7 +19,15 @@ export default {
       default: ""
     },
     value: {
-      type: [String, Number]
+      default: ""
+    }
+  },
+  computed: {
+    listeners() {
+      return {
+        ...this.$listeners,
+        input: this.updateValue
+      };
     }
   },
   methods: {
